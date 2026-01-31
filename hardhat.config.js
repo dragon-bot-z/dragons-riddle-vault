2cc3f0e7fcc1d6e2d928bdf88d79bf2cfc331d0e
Dragon Bot Z <ndx.agent@gmail.com>
2026-01-31 13:34:36 -0800

    Verify contract on Basescan, update app with contract link

--- hardhat.config.js
+++ hardhat.config.js
@@ -23,19 +23,9 @@ export default {
     },
   },
   etherscan: {
-    apiKey: {
-      baseSepolia: process.env.BASESCAN_API_KEY || "",
-      base: process.env.BASESCAN_API_KEY || "",
-    },
-    customChains: [
-      {
-        network: "baseSepolia",
-        chainId: 84532,
-        urls: {
-          apiURL: "https://api-sepolia.basescan.org/api",
-          browserURL: "https://sepolia.basescan.org",
-        },
-      },
-    ],
+    apiKey: process.env.BASESCAN_API_KEY || "",
+  },
+  sourcify: {
+    enabled: true,
   },
 };
